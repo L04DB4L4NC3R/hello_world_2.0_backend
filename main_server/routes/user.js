e59a01cab20527d12b8dd81c1fae4be95ab2abf0
angad <angadsharma1016@gmail.com>
2018-10-02 02:31:17 +0530

    item found

--- main_server/routes/user.js
+++ main_server/routes/user.js
@@ -41,11 +41,25 @@ router.post("/query",(req,res,next)=>{
  * 
  * @apiParam {string} name name of the user
  * @apiParam {string} obj object you are looking for
+ * 
+ * @apiParamExample {json} response
+ * {
+    "_id": "5bb2852280452113f2c816f2",
+    "timestamp": "12/12/12 8:00 pm",
+    "loc": "{lat:12.12334,lng:13.5678}",
+    "obj": "watch",
+    "found": false
+}   
  */
 router.post("/find",(req,res,next)=>{
     users.findOne({name:req.body.name,'items.obj':req.body.obj/*{$elemMatch:req.body.obj}*/})
     .then((d)=>{
-       res.json(d.items[0]);
+        if(!d)
+            next(new Error("No records found"));
+        if(!d.items[0].found)
+            res.json(d.items[0]);
+        else
+            res.json({message:"This item has already been found"});
     })
     .catch(next);
 });
@@ -60,6 +74,17 @@ router.post("/find",(req,res,next)=>{
  * @apiParam {string} name name of the user
  * @apiParam {Number} age age of the user
  * @apiParam {string} gender gender of the user
+ * 
+ * @apiParamExample {json} response
+ * {
+    "complimentary_users": [],
+    "_id": "5bb28943956e1d16a1372c65",
+    "name": "angad",
+    "age": 19,
+    "gender": "M",
+    "items": [],
+    "__v": 0
+}
  */
 router.post("/add",(req,res,next)=>{
     users.findOne({name:req.body.name})
@@ -74,6 +99,28 @@ router.post("/add",(req,res,next)=>{
         } else  
             res.json({message:"User already exists"});
     });
+});
+
+
+
+/**
+ * @api {post} /user/found object found
+ * @apiName object found
+ * @apiGroup user
+
+ * @apiParam {string} obj the name of the item
+ * @apiParam {string} name the name of the user
+ */
+router.post("/found",(req,res,next)=>{
+    users.findOne({name:req.body.name,'items.obj':req.body.obj})
+    .then((d)=>{
+        if(!d)
+            next(new Error("No records found"));
+        d.items[0].found = true;
+        d.save()
+        .then(()=>res.json({message:"record updated successfully"}))
+        .catch(next);
+    }).catch(()=>next(new Error("Error finding data")));
 });
 
 module.exports = router;
