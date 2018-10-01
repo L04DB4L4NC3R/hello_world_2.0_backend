3ad6f60d36946a07375a209c732e96553619d5d9
angad <angadsharma1016@gmail.com>
2018-10-02 03:33:10 +0530

    doctor and pharma schema

--- main_server/schema/schema.js
+++ main_server/schema/schema.js
@@ -15,3 +15,26 @@ module.exports.users = mongoose.model("user",new mongoose.Schema({
         default:[]
     }
 }));
+
+
+module.exports.doctors = mongoose.model("doctor",new mongoose.Schema({
+    name:String,
+    specialization:String,
+    contact:String,
+    rating:{
+        type:Number,
+        default:0.5
+    },
+    availability:String,
+    loc:String
+}));
+
+
+
+module.exports.pharmas = mongoose.model("pharma",new mongoose.Schema({
+    name:String,
+    loc:String,
+    contact:String,
+    availability:String
+}));
+
